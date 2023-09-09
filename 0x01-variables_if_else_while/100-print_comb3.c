92368d7125449de38f279f5d39b5a40fe6bbb04e
sherazme <sheraz.m.e@gmail.com>
2023-09-08 20:11:22 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num, num1;
+
+	for (num = 0; num < 10; num++)
+	{
+		for (num1 = num + 1; num1 <= 9; num1++)
+		{
+			putchar(num + 48);
+			putchar(num1 + 48);
+			if (num != 8 && num1 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
