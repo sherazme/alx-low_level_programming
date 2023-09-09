16786abde43f4bb59c757f7214522cd21df714e3
sherazme <sheraz.m.e@gmail.com>
2023-09-08 19:47:14 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,12 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	putchar(48 + num);
+	for (num = 1; num < 10; num++)
 	{
-		putchar(48 + num);
 		putchar(',');
 		putchar(' ');
+		putchar(48 + num);
 	}
 	putchar('\n');
 	return (0);
