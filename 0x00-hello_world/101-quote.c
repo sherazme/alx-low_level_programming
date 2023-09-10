76273e2b343501c65a21a7f6ce458c4624598961
sherazme <sheraz.m.e@gmail.com>
2023-09-09 20:11:46 -0700

    shell, I/O Redirection

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ *
+ * return always 0
+ */
+int main(void)
+{
+	echo("and that piece of art is useful\" -  Dora Korpar, 2015-10-19\n");
+	return (0);
+}
