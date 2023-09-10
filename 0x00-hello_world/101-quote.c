9df81a5afecf618a101da3e6ab6e022c5736bbbc
sherazme <sheraz.m.e@gmail.com>
2023-09-09 20:39:49 -0700

    shell, I/O Redirection

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	echo("and that piece of art is useful\" -  Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" -  Dora Korpar, 2015-10-19\n",60);
 	return (0);
 }
