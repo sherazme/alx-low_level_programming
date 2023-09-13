0b91383ca137ca7239405bbd22088f8c9ce362a4
sherazme <sheraz.m.e@gmail.com>
2023-09-12 17:57:12 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Check description
+ * @n: number
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	if (n > 0)
+		last = n % 10;
+	else
+		if (n < 0)
+			last = n % 10 * -1;
+		else
+			last = 0;
+	return (last);
+}
