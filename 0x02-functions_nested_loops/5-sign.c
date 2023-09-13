64969adbdeb16bf4628ae050d8890b4cd96fb19a
sherazme <sheraz.m.e@gmail.com>
2023-09-12 17:27:54 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_sign - Entry point
+* @n: parameter
+*
+* Return: Always 0 (Success)
+*/
+int print_sign(int n)
+{
+	int c;
+
+	if (n > 0)
+		c = 1;
+	else
+		if (n == 0)
+			c = 0;
+		else
+			c = -1;
+	return (c);
+}
