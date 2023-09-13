07b209e9ecba2134b91b16d0cf8340f087f6c909
sherazme <sheraz.m.e@gmail.com>
2023-09-12 19:41:21 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
-* print_alphabet - Entry point
+* print_alphabet_x10 - Entry point
 *
 * Return: Always 0 (Success)
 */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char c;
 	int i;
