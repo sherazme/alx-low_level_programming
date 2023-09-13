b92e8fda8573670c326fca8edacda1f4c9c2e888
sherazme <sheraz.m.e@gmail.com>
2023-09-12 19:29:47 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,55 +1,37 @@
-#include <unistd.h>
-#include "main.h"
+#include <stdio.h>
+#include "mainn.h"
 
 /**
- * print_to_98 - Check description
- * Description: It prints the word Holberton, followed by a new line.
- * @n: number
- * Return: 0.
+ * print_to_98 - Check Holberton
+ * @n: A input integer
+ * Description: function that prints all natural numbers
+ * from n to 98, followed by a new line.
+ * Return: Nothing
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n <= 98)
 	{
 		for (; n <= 98; n++)
 		{
-			if (n < 10)
-			{
-				_putchar(n + '0');
-			}
-			else
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-			}
+			printf("%d", n);
+
 			if (n == 98)
-			{
 				continue;
-			}
-			_putchar(',');
-			_putchar(' ');
+			printf(", ");
 		}
+		printf("\n");
 	}
 	else
-		if  (n == 98)
+	{
+		for (; n >= 98; n--)
 		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
 		}
-		else
-		{
-			for (; n >= 98; n--)
-			{
-				else
-				{
-					_putchar((n / 100) + '0');
-					_putchar((n / 10) + '0');
-					_putchar((n % 10) + '0');
-				}
-				if (n == 98)
-					continue;
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
+		printf("\n");
+	}
 }
