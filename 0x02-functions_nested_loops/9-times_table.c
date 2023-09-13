0c8567c5337bf446483693228edc15509d5e7d3f
sherazme <sheraz.m.e@gmail.com>
2023-09-12 18:39:26 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*  times_table - Entry point
+*
+* Return: Always 0 (Success)
+*/
+void times_table(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			_putchar(x * y + '0');
+
+		}
+		_putchar('\n');
+	}
+}
