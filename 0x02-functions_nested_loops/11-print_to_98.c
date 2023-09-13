51f4e71ff5277682b38651209be9101073454da6
sherazme <sheraz.m.e@gmail.com>
2023-09-12 19:27:00 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,14 +14,18 @@ void print_to_98(int n)
 		for (; n <= 98; n++)
 		{
 			if (n < 10)
+			{
 				_putchar(n + '0');
+			}
 			else
 			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
 			}
 			if (n == 98)
+			{
 				continue;
+			}
 			_putchar(',');
 			_putchar(' ');
 		}
