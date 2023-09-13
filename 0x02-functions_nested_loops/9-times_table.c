c71810028dbdad410d45571a3b62740958deb651
sherazme <sheraz.m.e@gmail.com>
2023-09-12 19:48:55 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,9 +15,13 @@ void times_table(void)
 		for (y = 0; y <= 9; y++)
 		{
 			r = x * y;
-			_putchar((r / 10) + '0');
-			_putchar((r % 10) + '0');
-
+			if (r < 10)
+				_putchar(r + '0');
+			else
+			{
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+			}
 			if (y != 9)
 			{
 				_putchar(',');
