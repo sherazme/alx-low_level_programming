2785bce94abb016ad1b75b6d6261860293612f41
sherazme <sheraz.m.e@gmail.com>
2023-09-12 19:10:21 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * @n: number
+ * Return: 0.
+ */
+void print_to_98(int n)
+{
+
+	for (; n <= 98; n++)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		if (n == 98)
+			continue;
+		_putchar(',');
+		_putchar(' ');
+	}
+}
