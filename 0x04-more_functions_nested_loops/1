e0885bf0cbc7530d6cccbef6d232cdc2d9f46dd0
sherazme <sheraz.m.e@gmail.com>
2023-09-13 23:30:33 -0700

    functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/1
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - Check characters
+ * Description: function uses _putchar function to print
+ * @n: the number of times the character _ should be printed
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - Check characters
+ * Description: function uses _putchar function to print
+ * @n: the number of times the character _ should be printed
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+*/
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
