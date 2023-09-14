0c2b9503500058ca4169c9ac836d38f74d3bf9c5
sherazme <sheraz.m.e@gmail.com>
2023-09-13 18:14:33 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int r, x;
+
+	for (r = 0; r < 1024; r++)
+	{
+		if (r % 3 == 0 || r % 5 == 0)
+			x += r;
+	}
+	printf(x);
+	return (0);
+}
