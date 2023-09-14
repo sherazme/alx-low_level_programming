fd8950a80e69d4d8d7c9f6e42797a0a074cb9c06
sherazme <sheraz.m.e@gmail.com>
2023-09-13 18:57:16 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int r, x, y, z;
+
+	x = 1;
+	y = 2;
+	printf("%lu", x);
+	printf(", %lu", y);
+	for (r = 2; r < 98; r++)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+		printf(", %lu", y);
+	}
+	printf("\n");
+	return (0);
+}
