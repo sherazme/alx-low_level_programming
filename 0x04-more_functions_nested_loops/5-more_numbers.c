6df0b9ec0cd7541dc7d5f339e419b4e100a600a6
sherazme <sheraz.m.e@gmail.com>
2023-09-14 19:53:04 -0700

    functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,28 +1,26 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * more_numbers - Check characters
- * Description: function uses _putchar function to print
- * Return: nothing
-*/
+  * more_numbers - Print 10 times the numbers since 0 up to 14
+  *
+  * Return: 10 times of the numbers since 0 up to 14
+  */
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j <= 9)
-			_putchar(j + '0');
-			else
+			if (j > 9)
 			{
-				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
+				_putchar((j / 10) + '0');
 			}
-			/*_putchar(' ');*/
+
+			_putchar((j % 10) + '0');
 		}
+
 		_putchar('\n');
 	}
 }
