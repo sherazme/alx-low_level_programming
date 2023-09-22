c30244296bf31840877c9a2fa357b343d2400fe2
sherazme <sheraz.m.e@gmail.com>
2023-09-21 18:49:28 -0700

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncat - Check characters
+ * @dest: An input character
+ * @src: source
+ * @n: num of bytes
+ * Description: function swap words
+ * Return: destination
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *str = dest, *srctemp = src;
+	int i = 0, strlength = 0;
+
+	while (*dest)
+		dest++;
+
+	while (*src)
+	{
+		strlength++;
+		src++;
+	}
+	src = srctemp;
+	for ( ; i < n; i++)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (str);
+}
