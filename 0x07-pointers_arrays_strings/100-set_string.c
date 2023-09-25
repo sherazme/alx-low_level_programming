1db68d649199f3aaff88ac6337a596dc2c3817df
sherazme <sheraz.m.e@gmail.com>
2023-09-24 22:47:56 -0700

    Even More pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_string - Check characters
+ * @s: An input character
+ * @to: bytes
+ * Description: function swap words
+ * Return: destination
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
