642c3cd2f9eb54a3a30c17730d4392096ebca628
sherazme <sheraz.m.e@gmail.com>
2023-09-24 19:38:41 -0700

    More pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - Check characters
+ * @n: An input character
+ * Description: function uses _putchar function to print
+ * Return: void
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
