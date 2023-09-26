9ccc6ac036512d6012bb76a9f845675237d16222
sherazme <sheraz.m.e@gmail.com>
2023-09-25 22:36:08 -0700

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2- Check characters
+ * @str: A n input character
+ * Description: function uses _putchar function to print
+ * Return: length
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
