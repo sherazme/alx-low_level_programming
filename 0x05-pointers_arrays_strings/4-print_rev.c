701acde39539ba5b6f0a07273450d6e4f1f48e67
sherazme <sheraz.m.e@gmail.com>
2023-09-25 22:00:52 -0700

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - Check characters
+ * @s: A n input character
+ * Description: function uses _putchar function to print
+ * Return: length
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		/*str++;*/
+		i--;
+	}
+	_putchar('\n');
+}
