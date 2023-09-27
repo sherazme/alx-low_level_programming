b40af4ee3fe2f8db07fc5a6757f5f790709ec347
sherazme <sheraz.m.e@gmail.com>
2023-09-26 18:33:18 -0700

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - Check characters
+ * @str: A n input character
+ * Description: function uses _putchar function to print
+ * Return: length
+*/
+void puts_half(char *str)
+{
+	int j = 0, i = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	i = j / 2;
+	while (i < j)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
