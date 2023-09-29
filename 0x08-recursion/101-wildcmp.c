325430b6fc7ee1011b1b4d24e04471aa0721e9c9
sherazme <sheraz.m.e@gmail.com>
2023-09-28 19:45:51 -0700

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,8 +2,9 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Check characters
- * @s: A n input character
+ * wildcmp - Check characters
+ * @s1: A n input character
+ * @s2: A n input character
  * Description: function uses _putchar function to print
  * Return: length
 */
@@ -13,7 +14,7 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2) || wildcmp(s1 + 1, s2 + 1)))
 		return (1);
 	if (*s2 == '*' && *(s1 + 1) && *s2)
 		return (0);
