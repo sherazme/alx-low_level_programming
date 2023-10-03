ace681b27be246a8fe91f8a6ce4d20fbd706ef2c
sherazme <sheraz.m.e@gmail.com>
2023-10-02 18:32:33 -0700

    C - argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: argc
+ * @argv: argv
+ * Description: prints file name
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 2)
+	{
+		i = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", i);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+
+	}
+}
