2eb5e67f2d9d412bd668a1eb4c44e93ad591548b
sherazme <sheraz.m.e@gmail.com>
2023-10-04 19:29:43 -0700

    malloc and free

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - function
+ * @str: value
+ * Return: pointer
+ */
+char **strtow(char *str)
+{
+char **words;
+	int i, j, wc, n, start, len, index = 0;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ' && (str[i - 1] == ' ' || i == 0))
+			wc++;
+	}
+	if (wc == 0)
+		return (NULL);
+
+	words = malloc(sizeof(char *) * (wc + 1));
+	if (words == NULL)
+		return (NULL);
+	words[wc] = NULL;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ' && (str[i - 1] == ' ' || i == 0))
+		{
+			start = i;
+			len = 0;
+			while (str[i] != ' ' && str[i])
+			{
+				len++;
+				i++;
+			}
+			words[index] = malloc(sizeof(char) * (len + 1));
+			if (words[index] == NULL)
+			{
+				for (n = 0; n < len; n++)
+					free(words[n]);
+				free(words);
+				return (NULL);
+			}
+			for (j = 0; j < len; j++)
+				words[index][j] = str[start + j];
+			words[index][j] = '\0';
+			index++;
+		}
+	}
+	return (words);
+}
