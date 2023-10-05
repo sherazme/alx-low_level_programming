1e8a7b5491104cf6f0464618d7f7fd5c610a0aad
sherazme <sheraz.m.e@gmail.com>
2023-10-04 20:36:40 -0700

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,41 +7,16 @@
  * @s2: A n input character
  * Description: function uses _putchar function to print
  * Return: length
-*/
+ */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (!*s1 && !*s2)
 		return (1);
-	else if (*s1 == '\0' || *s2 == '\0')
-	{
-		if (*s1 == '\0' && *s2 == '*')
-			return wildcmp(s1, ++s2);
-		else if (*s1 == '*' && *s2 == '\0')
-			return wildcmp(++s1, s2);
-		return (0);
-	}
 	if (*s1 == *s2)
-	{
-		return wildcmp(++s1, ++s2);
-	}
-	else if (*s1 == '*')
-	{
-		if (*(s1 + 1) == '*')
-			return wildcmp(++s1, s2);
-		else
-		{
-			return wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2);
-		}
-	}
-	else if (*s2 == '*')
-	{
-		if (*(s2 + 1) == '*')
-			return wildcmp(s1, ++s2);
-		else
-		{
-			return wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2);
-		}
-	}
-
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+		return (1);
+	if (*s2 == '*' && *(s1 + 1) && *s2)
+		return (0);
 	return (0);
 }
