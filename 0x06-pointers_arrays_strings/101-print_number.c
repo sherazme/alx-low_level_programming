d26aacad3fa6b1bd037df94bfed61146dcf250c3
sherazme <sheraz.m.e@gmail.com>
2023-10-10 20:22:22 -0700

    pointers arrays strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,22 +10,18 @@
 */
 void print_number(int n)
 {
+	unsigned int x;
+
 	if (n < 0)
+	{
 		_putchar('-');
-	if (n / 1000 > 0)
-	{
-		_putchar(n / 1000 + '0');
-		n = n % 1000;
+		n *= -1;
 	}
-	if (n / 100 > 0)
-	{
-		_putchar(n / 100 + '0');
-		n = n % 100;
-	}
-	if (n / 10 > 0)
-	{
-		_putchar(n / 10 + '0');
-		n = n % 10;
-	}
-	_putchar('\n');
+
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
 }
