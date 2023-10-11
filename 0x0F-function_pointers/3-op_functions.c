6ce3ae200dfc573d83712c79b3afaef134213fc1
sherazme <sheraz.m.e@gmail.com>
2023-10-10 21:34:15 -0700

    function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * op_add - add function
+ * @a: first number
+ * @b: second number
+ * Return: addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_mul - mul function
+ * @a: first number
+ * @b: second number
+ * Return: multi
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_sub - sub function
+ * @a: first number
+ * @b: second number
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_div - div function
+ * @a: first number
+ * @b: second number
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - mod function
+ * @a: first number
+ * @b: second number
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error");
+		exit(100);
+	}
+	return (a % b);
+}
