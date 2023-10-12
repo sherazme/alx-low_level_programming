ca5d3a24f1518b626c89b5b9c1d703e80ad8a6cc
sherazme <sheraz.m.e@gmail.com>
2023-10-11 19:00:31 -0700

    function pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: number of args
+ * @argv: args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int byte, i = 0;
+	unsigned char *func;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	byte = atoi(argv[1]);
+	if (byte < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	func = (unsigned char *)main;
+	
+	if (byte > 0)
+	{
+		while (i < byte--)
+			printf("%02hhx ", func[i++]);
+		printf("%hhx\n", func[i]);
+	}
+	return (0);
+}
