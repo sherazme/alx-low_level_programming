d01d18c547711391a9fcc277fe42f0577ed43893
sherazme <sheraz.m.e@gmail.com>
2023-10-19 18:51:21 -0700

    singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -31,4 +31,19 @@ list_t *add_node(list_t **head, const char *str)
 
 	return (new);
 }
+/**
+ * _strlen - Calculate lenght
+ *
+ * @str: String
+ *
+ * Return: lenght of a string.
+ */
+int _strlen(char *str)
+{
+	int len;
 
+	for (len = 0; str[len]; len++)
+		;
+
+	return (len);
+}
