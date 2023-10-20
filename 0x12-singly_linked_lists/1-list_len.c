e10a2091c49ee71cb9a8515ed472ca1aa02eff16
Shiraz <sheraz.m.e@gmail.com>
2023-10-20 03:19:33 +0200

    singly linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len -  Calculat lenght
+ * @h: Pointer
+ * Return: number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *current;
+
+	current = h;
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+
+	return (count);
+}
