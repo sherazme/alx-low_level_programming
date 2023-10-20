3c5e02fa7556f985c821ff3cea57107c36f55229
Shiraz <sheraz.m.e@gmail.com>
2023-10-20 03:37:46 +0200

    singly_linked_lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - Free list.
+ *
+ * @head: Head
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		current = head->next;
+		free(head->str);
+		free(head);
+		head = current;
+	}
+}
