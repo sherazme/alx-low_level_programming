4b11611e824187197e5497060d4bb5ef563c9252
Shiraz <sheraz.m.e@gmail.com>
2023-10-20 03:29:15 +0200

    singly_linked_lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * add_node - Add node
+ * @head: Pointer
+ * @str: String
+ *
+ * Return: address
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	char *string;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	string = strdup(str);
+	if (string == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->str = string;
+	new->len = _strlen(string);
+
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
+
+/**
+ * _strlen - lenght of string.
+ * @str: String
+ * Return: lenght
+ */
+ 
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len]; len++)
+		;
+
+	return (len);
+}
