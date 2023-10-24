a7a17ee77f0ac1a32f959c9bfd38637a2ae210d8
Shiraz <sheraz.m.e@gmail.com>
2023-10-24 06:36:34 +0200

    more singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		current = (*head)->next;
 		free(*head);
 		*head = current;
-		result = 1;
+		return (1);
 	}
 	
 	prev = NULL;
