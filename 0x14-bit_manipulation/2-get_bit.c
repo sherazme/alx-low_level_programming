d3a49527cb77b0660ce67959bd5ede104a602dbf
Shiraz <sheraz.m.e@gmail.com>
2023-10-27 04:29:42 +0200

    bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,13 +9,14 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int max = 0x01;
+	unsigned long int mask = 1;
 
-	max <<= index;
-	if (max == 0)
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
 
-	if ((n & max))
+	mask <<= index;
+
+	if (mask & n)
 		return (1);
 	else
 		return (0);
