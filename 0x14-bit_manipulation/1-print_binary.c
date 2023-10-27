9a49eefbf2dd03c8fbe0d8d3a209e938537e7eb0
Shiraz <sheraz.m.e@gmail.com>
2023-10-27 03:29:56 +0200

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,22 +8,25 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int flag = 0, max = 32768; /* 1000 0000 0000 0000 */
+	unsigned long int i, mask = 1;
+	int flag = 0;
 
-	if (n == 0)
+	mask <<= (sizeof(unsigned long int) * 8 - 1);
+
+	for (i = 0; i < sizeof(unsigned long int) * 8; i++)
 	{
-		_putchar('0');
-		return;
-	}
-	while (max)
-	{
-		if (flag == 1 && (n & max) == 0)
-			_putchar('0');
-		else if ((n & max) != 0)
+		if (mask & n)
 		{
 			_putchar('1');
 			flag = 1;
 		}
-		max >>= 1;
+		else
+		{
+			if (flag || i == sizeof(unsigned long int) * 8 - 1)
+				_putchar('0');
+		}
+
+		mask >>= 1;
+	}
 	}
 }
