a9cd6f7211907bcab99db02e1ed237ee0cc9a7d5
Shiraz <sheraz.m.e@gmail.com>
2023-10-31 01:28:46 +0200

    File descriptors and permissions

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,8 +3,8 @@
 /**
  * read_textfile - read text file
  * @filename: file to read from
- * @letter: number of letters it should read and print
- * Returns: the actual number of letters it could read and print
+ * @letters: number of letters it should read and print
+ * Return: the actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
