a5441e35a84c128fcdfaacb25d3e70a510ddb114
Shiraz <sheraz.m.e@gmail.com>
2023-10-31 03:22:32 +0200

    File descriptors and permissions

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,18 +12,18 @@ int main(int ac, char **av)
 	
 	if (ac != 3)
 		exit_and_print("Usage: cp file_from file_to\n", av[0], 97);
-		
+	
 	source = read_textfile(av[1], 114);
 	if (source == -1)
 		exit_and_print("Error: Can't read from file %s\n", av[1], 98);
-	
+
 	destination = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (destination == -1)
 		exit_and_print("Error: Can't write to %s\n", av[2], 99);
-	
+
 	while ((byteRead = read(source, buffer, 1024)) > 0)
 	{
-		byteWrite = write(dst, buffer, byteRead);
+		byteWrite = write(destination, buffer, byteRead);
 		if (byteWrite == -1)
 			exit_and_print("Error: Can't write to %s\n", av[2], 99);
 	}
@@ -32,12 +32,12 @@ int main(int ac, char **av)
 		exit_and_print("Error: Can't read from file %s\n", av[1], 98);
 
 	if (close(source) == -1)
-		exit_and_print("Error: Can't close fd %d\n", source, 100);
-	
+		exit_and_print("Error: Can't close fd %d\n", (int)source, 100);
+
 	if (close(destination) == -1)
-		exit_and_print("Error: Can't close fd %d\n", destination, 100);
+		exit_and_print("Error: Can't close fd %d\n", (int)destination, 100);
 	return (0);
-	
+
 }
 
 /**
@@ -47,7 +47,7 @@ int main(int ac, char **av)
  * @code: exit code
  * Return: void
  */
- 
+
 void exit_and_print(char *msg, char *argument, int code)
 {
 	dprintf(STDERR_FILENO, msg, argument);
