d8f1609b76b4c4c1d4356fd9a59659af16734280
Shiraz <sheraz.m.e@gmail.com>
2023-10-31 02:23:41 +0200

    File descriptors and permissions

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_APPEND | O_EXCL, 0600);
+	fd = open(filename, O_CREAT | O_APPEND | O_EXCL);
 	if (fd == -1)
 		return (-1);
 
